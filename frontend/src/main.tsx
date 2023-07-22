5ee9d40bf428e0bbf906708e0a4ba958268145aa
Julio M Cruz <julio.cruz@eb-ms.net>
2023-07-22 07:33:41 -0400

    Fix react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@fontsource/raleway/400.css";
 import "@fontsource/open-sans/700.css";
 import { LandingPage } from "./LandingPage.js";
+import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
 
 const theme = extendTheme({
   styles: {
@@ -27,7 +28,9 @@ const theme = extendTheme({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
