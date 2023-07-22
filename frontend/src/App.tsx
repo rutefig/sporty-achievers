5e3d62bd1b7e1540974ba349b5ba35031c083cd4
Julio M Cruz <julio.cruz@eb-ms.net>
2023-07-22 08:01:54 -0400

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,18 @@ import { LandingPage } from "./LandingPage";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
 import  Profile  from "./Profile";
+import { Layout } from "./Layout";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Layout />}>
           <Route index element={<LandingPage />} />
           <Route path="profile" element={<Profile />} />
-      </Routes>    </>
+        </Route>
+      </Routes>   
+    </>
   );
 }
 
