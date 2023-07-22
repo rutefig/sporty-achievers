16bcd06b05db7939da8ffff5c9a9180a32b632f4
Pauline Barnades <pauline.barnades@stenaline.com>
2023-07-22 14:00:54 +0200

    profile UI fix

--- frontend/src/Profile.tsx
+++ frontend/src/Profile.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import {
-  Box,
-  Flex,
-  Avatar,
-  Text,
-  Container,
-  Heading,
-  Card,
-} from "@chakra-ui/react";
+import { Box, Flex, Avatar, Heading, Stack, VStack } from "@chakra-ui/react";
 
 // interface ProfileProps {
 //   name: string;
 //   imageUrl: string;
 // }
 
-const Profile: React.FC = () => {
+export const Profile: React.FC = () => {
   const imagebg =
     "https://images.unsplash.com/photo-1560090995-01632a28895b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3538&q=80";
   return (
@@ -33,15 +25,24 @@ const Profile: React.FC = () => {
               src="https://bit.ly/dan-abramov"
             />
           </Flex>
-          <Text fontSize="xl" ml={4}>
+          <Heading size={"lg"} ml={4}>
             {/* {name} name */}
             Michael Jones
-          </Text>
+          </Heading>
         </Flex>
       </Box>
 
-      <Flex justify="space-between" width="100%" flexDirection={"column"}>
-        <Container>
+      <Stack direction={["column", "row"]} spacing="24px">
+        <Box
+          maxW="900px"
+          maxH="400px"
+          border="2px"
+          borderColor="gray.200"
+          borderRadius={10}
+          mb={20}
+          mt={10}
+        >
+          <Heading>About Mickael</Heading>
           Michael Phelps is a legendary American swimmer widely regarded as one
           of the greatest athletes in Olympic history. Born on June 30, 1985, in
           Baltimore, Maryland, he quickly rose to prominence as a swimming
@@ -53,38 +54,46 @@ const Profile: React.FC = () => {
           potential. However, it was in the 2004 Athens Olympics that he truly
           emerged as a force to be reckoned with, securing six gold medals and
           two bronze medals, a remarkable achievement for any athlete.
-        </Container>
-      </Flex>
-      <Flex flexDirection={"row"} justifyContent="space-between">
-        <Box>
-          <Box
-            width="800px"
-            height="400px"
-            border="2px"
-            borderColor="gray.200"
-            borderRadius={10}
-            mb={20}
-          >
-            <Heading size={"lg"}>Sponsors</Heading>
-            {/* Content of the first box */}
-          </Box>
-          <Card>something</Card>
         </Box>
-      </Flex>
-
-      <Box
-        width="800px"
-        height="400px"
-        border="2px"
-        borderColor="gray.200"
-        borderRadius={10}
-        mb={15}
-      >
-        {/* Content of the second box */}
-        <Heading size={"lg"}>Medals</Heading>
-      </Box>
+        <Box
+          maxW="900px"
+          maxH="400px"
+          border="2px"
+          borderColor="gray.200"
+          borderRadius={10}
+          mb={20}
+          mt={10}
+        >
+          bdabdgorgnopergnomepogmopergmeropgmäemero
+        </Box>
+      </Stack>
+      <VStack spacing={4} align="stretch">
+        <Box
+          width="800px"
+          height="400px"
+          border="2px"
+          borderColor="gray.200"
+          borderRadius={10}
+          mb={20}
+        >
+          <Heading size={"lg"}>Sponsors</Heading>
+          {/* Content of the first box */}
+        </Box>
+        <Box
+          width="800px"
+          height="400px"
+          border="2px"
+          borderColor="gray.200"
+          borderRadius={10}
+          mb={15}
+        >
+          {/* Content of the second box */}
+          <Heading size={"lg"}>Medals</Heading>
+        </Box>
+        <Box h="40px" bg="pink.100">
+          3
+        </Box>
+      </VStack>
     </>
   );
 };
-
-export default Profile;
